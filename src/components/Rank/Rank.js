50b8bc9fc17efa0d0bbfc6158b96f2a88abb17a3
Hero988 <gissuliman@gmail.com>
2022-10-23 14:47:43 +0100

    new files

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -6,7 +6,7 @@ const Rank =  ({ name, entries }) => {
      return (
           <div>
             <div className='white f3 b'>
-              {`${name}, your current entry count is...`}
+              {`hello, your current entry count is...`}
             </div>
             <div className='white f1 b'>
             {entries}
