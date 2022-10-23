5cbd73e1e237ccefecef68018ddd51e4cc3e970b
Hero988 <gissuliman@gmail.com>
2022-10-23 14:53:11 +0100

    new files

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,14 +4,13 @@ import React from 'react';
 // Pass two props you need into the Rank component then display the message: "${name} , your current rank is...${entries}" :
 const Rank =  ({ name, entries }) => {
      return (
-          <div>
-            <div className='white f3 b'>
-              {`hello, your current entry count is...`}
-            </div>
-            <div className='white f1 b'>
-            {entries}
-            </div>
-          </div>
+      <div>
+      <div className='white f1 b'>
+        {`Hello, wlecome back!`}
+      </div>
+      <div className='white f1 b'>
+      </div>
+    </div>
      )
 }
 
